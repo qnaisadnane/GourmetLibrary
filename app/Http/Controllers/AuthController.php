52ad7b28eb300ea70cdc0cb3da2b1cd01662f22f
adnane <qnaisadnane@gmail.com>
2026-03-17 10:32:57 +0000

    le teste de l'authentification est passé avec succes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,8 +11,8 @@ class AuthController extends Controller
     public function register(Request $request){
              $request->validate([
                 'name'=>'required',
-                'email'=>'required | unique:users',
-                'password'=>'required|min 8'
+                'email'=>'required |email | unique:users',
+                'password'=>'required|min:8'
              ]);
             $user = User::create([
                 'name'=>$request->name,
@@ -20,7 +20,7 @@ class AuthController extends Controller
                 'password'=>Hash::make($request->password)
             ]);
 
-             $token = createToken('api-token')->plainTextToken;
+             $token = $user->createToken('api-token')->plainTextToken;
 
              return [
                 'user'=>$user,
@@ -32,7 +32,7 @@ class AuthController extends Controller
 
             if(!$user || !Hash::check($request->password,$user->password)){
             return response()->json([
-                'message'=>'Invalid credentials'
+                'message'=>'Invalid user'
             ],401);
         }
 
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+
 Route::get('/books/popular', [BookController::class,'popular']);
 Route::get('/books/latest', [BookController::class,'latest']);
 Route::get('/books/search', [BookController::class,'search']);
