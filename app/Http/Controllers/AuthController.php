8abe14177ccf3180bcd81b802de502ffca5d6fa5
adnane <qnaisadnane@gmail.com>
2026-03-17 05:00:57 +0000

    l'ajout de l'authentification

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+             $request->validate([
+                'name'=>'required',
+                'email'=>'required | unique:users',
+                'password'=>'required|min 8'
+             ]);
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>Hash::make($request->password)
+            ]);
+
+             $token = createToken('api-token')->plainTextToken;
+
+             return [
+                'user'=>$user,
+                'token'=>$token
+             ];
+    }
+    public function login(Request $request){
+            $user = User::where('email',$request->email)->first();
+
+            if(!$user || !Hash::check($request->password,$user->password)){
+            return response()->json([
+                'message'=>'Invalid credentials'
+            ],401);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return [
+            'user'=>$user,
+            'token'=>$token
+        ];
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return ['message'=>'logged out'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 use App\Models\Category;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 
 Route::get('/books/popular', [BookController::class,'popular']);
 Route::get('/books/latest', [BookController::class,'latest']);
